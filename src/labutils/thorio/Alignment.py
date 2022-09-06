bafa19615724733184cb7a7a7b7a03e4ace1e695
markdawn <albama92@gmail.com>
2022-09-06 15:18:25 +0200

    Fixed alignment between t-Z added ability to plot aligment images

--- src/labutils/thorio/Alignment.py
+++ src/labutils/thorio/Alignment.py
@@ -11,7 +11,11 @@ class AlignableRigidPlaneData:
         self.shifts = None
         super().__init__(*args, **kwargs)
 
-    def _align(self, downZ=6):
+    def _align(self, downZ=6, crop_area=100, plot=False):
+        if plot:
+            import matplotlib.patches as patches
+            from matplotlib import pyplot as plt
+            from ..plot import AutoFigure
         if self.alignTo is None:
             raise ValueError("set target alignment to a Zexp")
         try:
@@ -25,17 +29,37 @@ class AlignableRigidPlaneData:
         print(f"aligning {self.path} to {self.alignTo.path}")
         # assert(T.px2um == self.Z.px2um[1])
         norm_Tstack = _norm_u16stack2float(self.meanImg)
-        shifts = []
-        for i, plane in enumerate(norm_Tstack):
+        shifts = [(0,0,0)]
+        #mx = self.meanImg.min()
+        for i, plane in enumerate(norm_Tstack, ):
             plane = np.stack([plane])
             lowshift = feature.match_template(lown_Zstack, plane).argmax()
             slz = slice(max(0,downZ*(lowshift - 2)), min(downZ*(lowshift + 2), norm_Zstack.shape[0]))
-            print(slz, lowshift, plane.shape, norm_Zstack[slz,...].shape)
+            # print(slz, lowshift, plane.shape, norm_Zstack[slz,...].shape)
             z = feature.match_template(norm_Zstack[slz, ...], plane).argmax() + slz.start
-            shifted_img = feature.match_template(self.alignTo.img[z], self.meanImg[i], pad_input=True,)
-            sls = [slice(zc-100, zc+100) for zc in map(lambda x: x//2,  shifted_img.shape)]
+            # shifted_img = feature.match_template(self.alignTo.img[z], self.meanImg[i], pad_input=True,constant_values=mx)
+            shifted_img = feature.match_template(self.meanImg[i], self.alignTo.img[z], pad_input=True, mode='minimum')
+            sls = [slice(zc-crop_area, zc+crop_area) for zc in map(lambda x: x//2,  shifted_img.shape)]
             shift = np.unravel_index(shifted_img[sls[0], sls[1]].argmax(), shifted_img[sls[0], sls[1]].shape)
-            shifts.append((z, *tuple(s + sl.start - d // 2 for s, sl, d in zip(shift, sls, self.meanImg.shape[1:]))))
+            shifts.append((z, *tuple(-(s + sl.start - d // 2) for s, sl, d in zip(shift, sls, self.meanImg.shape[1:]))))
+            if plot:
+                with AutoFigure(os.path.join(self.path, f"dbg_shift_{i+1}"),nrows=2) as (fig, ax):
+                    b=shifts[-1]
+                    axt, axz = ax
+                    axz.imshow(shifted_img)
+                    axz.add_patch(patches.Rectangle((sls[1].start, sls[0].start), crop_area*2, crop_area*2, edgecolor='r', facecolor='none'))
+                    axt.set_title(b)
+                    axt.imshow(self.meanImg[i])
+                # with AutoFigure(os.path.join(self.path, f"dbg_z_{i+1}"),nrows=2) as (fig, ax):
+                #     b=shifts[-1]
+                #     axt, axz = ax
+                #     axz.imshow(self.alignTo.img[(a:=int(b[0]))])#, vmax=1e4)
+                #     axz.axvline(self.meanImg.shape[-1]//2 + b[-1], color='b')
+                #     axz.axhline(self.meanImg.shape[-2]//2 + b[-2], color='b')
+                #     axt.set_title(b)
+                #     axt.imshow(self.meanImg[i])
+                plt.close('all')
+            print(f"plane {i+1} matches z={z} with shifts={shifts[-1][1:]}")
         self.shifts = np.stack(shifts)
         
     def transformPoints(self, points):
