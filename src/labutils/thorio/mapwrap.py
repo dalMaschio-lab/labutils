724a142ab18904933c6e6d0e01918ed6b8ec20a3
markdawn <albama92@gmail.com>
2022-12-09 16:21:34 +0100

    Critical fixes to mapwrap Added proper support for reduce operations

--- src/labutils/thorio/mapwrap.py
+++ src/labutils/thorio/mapwrap.py
@@ -3,47 +3,41 @@ import itertools
 
 
 class rawTseries(np.memmap):
-    def __new__(subtype, filename, shape, clips=None, flyback=None, transforms=None, chunksize=None, dtype=np.uint16, **kwargs):
+    def __new__(subtype, filename, shape, clips=None, flyback=None, chunksize=None, dtype=np.uint16, **kwargs):
         if len(shape) < 3:
             raise TypeError("Intended for 2D or 3D timeseries. First axis is always indicating a series of image sor volumes")
-        if flyback and len(flyback) != len(shape) - 1:
-            raise TypeError('flyback should be specfied for all axis except the first')
 
-        arr = np.memmap(filename, shape=shape, dtype=dtype, mode='r', **kwargs).view(subtype)
-        arr.clips = clips
+        if clips is None:
+            clips = Ellipsis
+        elif isinstance(clips, (tuple, list, np.ndarray)):
+            clips = tuple(clips)
+        else:
+            clips = (clips,)
+
+        arr = np.memmap(filename, shape=shape, dtype=dtype, mode='r', **kwargs)[clips].view(subtype)
+        arr.zeroiseven = clips
         arr.flyback = flyback
-        arr.transforms = transforms
         arr.chunksize = chunksize
         return arr
+
+    def __init__(subtype, filename, shape, clips=None, flyback=None, chunksize=None, dtype=np.uint16, **kwargs):
+        pass
 
     def __array_finalize__(self, obj):
         # we are in the middle of our __new__
         if obj is None: return
         # else we are being constructed somewhere else by viewcasting
-        self.clips = getattr(obj, 'clips', None)
         tmp = getattr(obj, 'flyback', None)
         self.flyback = tmp
-        self.transforms = getattr(obj, 'transforms', None)
+        self.zeroiseven = getattr(obj, 'zeroiseven', ...)
         self.chunksize = getattr(obj, 'chunksize', None)
 
     @property
-    def shape(self):
-        return tuple(
-            ((0 if c.stop > 0 else s) + c.stop) - ((0 if c.start > 0 else s) + c.start)
-            for s, c in zip(super().shape, self._clips)
-        ) if self._clips is not Ellipsis else super().shape
- 
-    @property
-    def clips(self):
-        return self._clips
-    @clips.setter
-    def clips(self, val):
-        if val in (None, Ellipsis):
-            self._clips = Ellipsis
-        elif len(val) == self.ndim and all(isinstance(c, slice) for c in val):
-            self._clips = val
-        else:
-            raise ValueError('clips should be a tuple for each axis of clipping slices')
+    def zeroiseven(self):
+        return self._zeroiseven
+    @zeroiseven.setter
+    def zeroiseven(self, val):
+        self._zeroiseven = tuple((idx.start%2 if idx.start is not None else 0) if isinstance(idx, slice) else idx%2 for idx in self._process_indexes(val))
 
     @property
     def flyback(self):
@@ -53,36 +47,65 @@ class rawTseries(np.memmap):
         if val is None:
             self._flyback = (0, ) * (self.ndim-1)
         elif len(val) == self.ndim - 1 and all(isinstance(c, int) for c in val):
-            if self._clips is Ellipsis:
-                self._flyback = val
-            else:
-                self._flyback = tuple(-v if c.start%2 else v for v, c in zip(val, self._clips))
+            self._flyback = val
         else:
             raise ValueError('flyback should be a tuple for each axis except the first')
+
+    def _process_indexes(self, ixs):
+        if ixs is ...:
+            return (slice(None), ) * self.ndim
+        elif type(ixs) is not tuple:
+            ixs = (ixs, ...)
+        try:
+            ell_idx = ixs.index(...)
+        except ValueError:
+            ell_idx = len(ixs)
+        pre, post = ixs[:ell_idx], ixs[ell_idx+1:]
+        return pre +  (slice(None), ) * (self.ndim - len(pre) - len(post)) + post
 
     def __getitem__(self, key):
         if key is None:
             return self
-        elif key is Ellipsis:
-            key = (slice(0, None), ...)
-        elif type(key) is not tuple:
-            key = (key, ...)
         
-        # slice first dimension
-        k0, k = key[0], key[1:]
-        if k0 is Ellipsis:
-            k0 = slice(0, None)
-            k = (..., *k)
-        elif type(k0) is int:
-            k0 = slice(k0, k0+1)
-        out = self.view(np.memmap)[self._clips][k0,]
+        key = self._process_indexes(key)
+        stages = []
+        assert(len(key)==self.ndim)
+        for k, s, f in zip(key, self.shape, (0,) + self._flyback):
+            if not f:
+                if type(k) is slice:
+                    startodd = k.start and (k.start if k.start >= 0 else s + k.start)%2
+                    st0 = slice(k.start - 1 if startodd else k.start, k.stop,)
+                    st1 = slice(1 if startodd else None, None, k.step)
+                elif type(k) is int:
+                    k = k if k >= 0 else s+k
+                    st0 = slice(k - 1 if k%2 else k, k+1,)
+                    st1 = k%2
+                elif isinstance(k, (list, tuple, np.ndarray)):
+                    k = np.asarray(k, )
+                    if k.ndim != 1:
+                        raise IndexError(f"cannot index an axis with a {k.ndim}d array")
+                    if k.dtype == bool:
+                        k = np.where(k)[0]
+                        st0 = slice(k.min(), k.max())
+                        st1 = k - k.min()
+                    elif k.dtype == int:
+                        st0 = slice(k.min(), k.max()+1)
+                        st1 = k - k.min()
+                    else:
+                        raise IndexError(f"Unsupported type for array indexing {k}")
+                else:
+                    raise IndexError(f"Unsupported type for array indexing {k}")
+            else:
+                st0 = slice(None)
+                st1 = k
+            stages.append((st0, st1))
+        
+        stages = tuple(zip(*stages))
+        out = self.view(np.memmap)[stages[0]]
 
         # sequential rolling logic to fix flybacks by using rolling logic
-        swap = False
-        for shift, axis in zip(self._flyback[::-1], range(out.ndim)):
+        for shift, axis, swap in zip(self._flyback[::-1], range(out.ndim), self.zeroiseven[::-1]):
             if shift:
-                if axis==(out.ndim-2) and k0.start is not None and k0.start%2 :
-                    swap = True
                 tmp = np.empty_like(out)
                 tmp[self._make_slicing_tuple(axis, slice(shift, None), even=swap)] = out[self._make_slicing_tuple(axis, slice(None, -shift), even=swap)]
                 tmp[self._make_slicing_tuple(axis, slice(None, shift), even=swap)] = out[self._make_slicing_tuple(axis, slice(-shift, None), even=swap)]
@@ -90,9 +113,7 @@ class rawTseries(np.memmap):
                 tmp[evens] = out[evens]
                 out = tmp
         
-        if type(key[0]) is int:
-            out = out[0]
-        return out[(slice(None), *k)] if out.ndim == self.ndim else out[k]
+        return out[stages[1]]
     
     @staticmethod
     def _make_slicing_tuple(axis, sl, even=False):
@@ -103,31 +124,41 @@ class rawTseries(np.memmap):
 
     def __array_ufunc__(self, ufunc, method, *inputs, **kwargs):
         if method == 'reduce':
-            axis = kwargs['axis'] if type(kwargs['axis']) is tuple else (kwargs['axis'], )
-            out = np.empty(tuple(s for i, s in enumerate(inputs[0].shape) if i not in axis), dtype=kwargs['dtype'] if kwargs['dtype'] else inputs[0].dtype)
+            if type(kwargs['axis']) is tuple:
+                axis = kwargs['axis'] 
+            elif kwargs['axis'] is None:
+                axis = tuple(range(inputs[0].ndim))
+            else:
+                axis= (kwargs['axis'], )
+            out = np.empty(
+                tuple(s for i, s in enumerate(inputs[0].shape) if i not in axis),
+                dtype=kwargs['dtype'] if kwargs['dtype'] else np.dtype(f"{inputs[0].dtype.byteorder}{inputs[0].dtype.kind}4"))
             out.fill(ufunc.identity)
             blocks = np.arange(0, inputs[0].shape[0], self.chunksize)
             for start, stop in zip(blocks, [*blocks[1:], None]):
-                print(start, stop)
                 arr = inputs[0][start:stop]
-                ufunc(out, ufunc.reduce(arr, axis), out=out)
+                if 0 in axis:
+                    ufunc(out, ufunc.reduce(arr, axis), out=out)
+                else:
+                    out[start:stop] = ufunc.reduce(arr, axis)
             return out
         else:
+            print(method, ufunc)
             return NotImplemented
 
-# a=np.memmap('test', dtype=np.uint16, shape=(20,8,12), mode='w+')
-# a[:]= np.arange(a.size).reshape(a.shape)
-# a.flush()
-# del a
-a = rawTseries('test', shape=(20,8,12), flyback=(1,3))
-a[...]
-#a[...,3]
-a[3, :, 1:3]
-a[3:4, ...]
-#a[(1,2,3,5), :]
-mean = np.sum(a, axis=0)
-mean2 = np.zeros(a.shape[1:], dtype=a.dtype)
-for s in a[:]:
-    mean2+=s
-
-mean = a.mean(axis=2)
+if __name__ == '__main__':
+    # a=np.memmap('test', dtype=np.uint16, shape=(20,8,12), mode='w+')
+    # a[:]= np.arange(a.size).reshape(a.shape)
+    # a.flush()
+    # del a
+    a = rawTseries('test.raw', shape=(20,8,12), flyback=(1,3), chunksize=30, clips=(slice(3, 10)))
+    assert(np.all(a[...] == a[:]))
+    assert(np.all(a[3, :, 1:3] == a[:][3, :, 1:3]))
+    assert(np.all(a[3:10:2, ...] == a[:][3:10:2, ...]))
+    assert(np.all(a[(2,3,5), :] == a[:][(2,3,5), :]))
+    assert(np.all(a.sum(axis=0) == a[:].sum(axis=0)))
+    assert(np.all(a.sum(axis=2) == a[:].sum(axis=2)))
+    assert(np.all(a.sum(axis=(1,2)) == a[:].sum(axis=(1,2))))
+    assert(np.all(a.mean() == a[:].mean()))
+    assert(np.all(np.diff(a, axis=1)==np.diff(a[:], axis=1)))
+    #a+ 3
